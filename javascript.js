c91bc9ddcd10ea240801782394de4cea22f8067c
Tovape <tonivalverde0@gmail.com>
2021-11-06 20:37:16 +0100

    a

--- javascript.js
+++ javascript.js
@@ -84,18 +84,17 @@ var arrLang = {
 	'webdeveloper': 'Web Developer',
 	'3dmodeling': '3D Modeling',
 	'softhardware': 'Software/Hardware Helpdesk',
-	'introduction-text': 	`Hi, Welcome to my portafolio webpage!
-							I like making webpages, whether it is Frontend or Backend, there's always something new to learn and a new skill to improve.
-							I also love 3d modeling and texture making
-							I've been interested in computer science from young, the way such complexity worked fascinated me, I still have to discover so much more sincethere are a lot of layers in the world of computer science.
-							I started learning programming in HTML, CSS and Javascript, I found them like the best way to start learning and getting my career in order.
-							In 2020, during the covid pandemic I started 3D Modeling as a hobbie and it changed my life, I used to procrastinate a lot and Blender changed it, I loved the way progress was made by joining vertices and it remarked my life.
-							
-							I'm currently making websites for small business for free in order to grow my portafolio and gain knowledge.
+	'introduction-text1': 	`Hi, Welcome to my portafolio webpage!`,
+	'introduction-text2': 	`I like making webpages, whether it is Frontend or Backend, there's always something new to learn and a new skill to improve.`,
+	'introduction-text3': 	`I also love 3d modeling and texture making`,
+	'introduction-text4': 	`I've been interested in computer science from young, the way such complexity worked fascinated me, I still have to discover so much more sincethere are a lot of layers in the world of computer science.`,
+	'introduction-text5': 	`I started learning programming in HTML, CSS and Javascript, I found them like the best way to start learning and getting my career in order.`,
+	'introduction-text6': 	`In 2020, during the covid pandemic I started 3D Modeling as a hobbie and it changed my life, I used to procrastinate a lot and Blender changed it, I loved the way progress was made by joining vertices and it remarked my life. I'm currently making websites for small business for free in order to grow my portafolio and gain knowledge.`,
+	'introduction-text7': 	`I like to think life is a train in rails, and our duty is making sure the train doesn't fall over, the more discipline, the more your train will shine.`,
+	'introduction-text8': 	`❝Jump back and get back a little more ❞`,
+	'introduction-text9':	`--The Qemists`,
 						
-							I like to think life is a train in rails, and our duty is making sure the train doesn't fall over, the more discipline, the more your train will shine.
-							❝Jump back and get back a little more❞
-							--The Qemists`,
+							
 	
 	// Right
 
@@ -131,17 +130,17 @@ var arrLang = {
 	'webdeveloper': 'Desarollador Web',
 	'3dmodeling': 'Modelador 3D',
 	'softhardware': 'Resoluciones Software/Hardware',
-	'introduction-text':	`Hola, bienvenidos a mi página web portafolio!
-							Me gusta hacer páginas web, ya sea la parte de frontend como la de backend, siempre hay algo nuevo por aprender y una habilidad nueva para mejorar. 
-							También me gusta el modelaje 3D y texturizado.
-							Empezé a interesarme en la informática desde joven, la forma en que los ordenadores funcionaban me fascinaba, todavía me falta aprender mucho ya que hay muchas capas en el mundo de las Ciencias de la Computación
-							Empezé aprendiendo programación gracias al lenguaje de HTML, CSS y Javascript, fue el mejor camino para empezar a construir una carrera professional
-							En 2020 durante la pandemia del Covid, empezé a modelar en 3d como hobby y me cambió la vida, Solía procrastinar mucho y Blender lo cambió, me encantó la forma en la que se veía el progreso a través del tiempo, de vértice a vértice, remarcó mi vida.
-							Actualmente estoy creando sitios web para pequeñas empresas de forma gratuita con el fin de ampliar mi portafolio y adquirir conocimientos.
-							Me gusta pensar que la vida es un tren sobre railes, y nuestro deber es asegurarnos de que el tren no se caiga, mientras más disciplina, más brillará nuestro tren.
-							❝Jump back and get back a little more❞
-							--The Qemists`,
-							
+	
+	'introduction-text1': 	`Hola, bienvenidos a mi página web portafolio!`,
+	'introduction-text2': 	`Me gusta hacer páginas web, ya sea la parte de frontend como la de backend, siempre hay algo nuevo por aprender y una habilidad nueva para mejorar.`,
+	'introduction-text3': 	`También me gusta el modelaje 3D y texturizado.`,
+	'introduction-text4': 	`Empezé a interesarme en la informática desde joven, la forma en que los ordenadores funcionaban me fascinaba, todavía me falta aprender mucho ya que hay muchas capas en el mundo de las Ciencias de la Computación`,
+	'introduction-text5': 	`Empezé aprendiendo programación gracias al lenguaje de HTML, CSS y Javascript, fue el mejor camino para empezar a construir una carrera professional.`,
+	'introduction-text6': 	`En 2020 durante la pandemia del Covid, empezé a modelar en 3d como hobby y me cambió la vida, Solía procrastinar mucho y Blender lo cambió, me encantó la forma en la que se veía el progreso a través del tiempo, de vértice a vértice, remarcó mi vida. Actualmente estoy creando sitios web para pequeñas empresas de forma gratuita con el fin de ampliar mi portafolio y adquirir conocimientos.`,
+	'introduction-text7': 	`Me gusta pensar que la vida es un tren sobre railes, y nuestro deber es asegurarnos de que el tren no se caiga, mientras más disciplina, más brillará nuestro tren.`,
+	'introduction-text8': 	`❝Jump back and get back a little more ❞`,
+	'introduction-text9':	`--The Qemists`,
+						
 	// Right
 
 	'introduction': 'Introducción',
@@ -176,17 +175,17 @@ var arrLang = {
 	'webdeveloper': 'Desenvolupador Web',
 	'3dmodeling': 'Modelador 3D',
 	'softhardware': 'Solucions Software/Hardware',
-	'introduction-text':	`Hola, benvinguts a la meva pàgina web portafoli!
-							M'agrada fer pàgines web, ja sigui la part de frontend com la de backend, sempre hi ha algo nou per descobrir i una nova habilitat per millorar. 
-							A més m'agrada el modelatje 3D i la texturació.
-							L'informàtica em va començar a interesar desde jove, la forma en la que els ordinadors funcionaven me fascinaba, encara em falta molt per aprendre ja que en aquest mon hi ha moltes capes
-							Vaig començar aprenent programació gràcies als idiomes HTML, CSS i Javascript, va ser la millor manera de aprendre i per construir una carrera professional
-							En 2020 durant la pandemia del Covid, vaig començar a fer models 3d com a hobby i em va cambiar la vida, solia procrastinar molt i Blender ho va cambiar, m'encantava la forma en la que es podía veure el progrés a través del temps, de vèrtex en vèrtex, va cambiar la meva vida a millor.
-							Actualment estic creant pàgines web per petites empreses de franc amb el fi d'ampliar el meu portafolis i conseguir més coneixements.
-							M'agrada pensar que vida és un tren sobre rails, i que el nostre deure es assegurar-nos de que aquest no caigui, mentre més disciplina, més brillarà el nostre tren.
-							❝Jump back and get back a little more❞
-							--The Qemists`,
-							
+	
+	'introduction-text1': 	`Hola, benvinguts a la meva pàgina web portafoli!`,
+	'introduction-text2': 	`M'agrada fer pàgines web, ja sigui la part de frontend com la de backend, sempre hi ha alguna cosa nova per descobrir i una nova habilitat per millorar.`,
+	'introduction-text3': 	`A més m'agrada el modelatge 3D i la texturació.`,
+	'introduction-text4': 	`La informàtica em va començar a interessar des de jove, la forma en que els ordinadors funcionaven em fascinava, encara em falta molt per aprendre, ja que en aquest món hi ha moltes capes.`,
+	'introduction-text5': 	`Vaig començar aprenent programació gràcies als idiomes HTML, CSS i Javascript, va ser la millor manera d'aprendre i per construir una carrera professional`,
+	'introduction-text6': 	`En 2020 durant la pandèmia del Covid, vaig començar a fer models 3d com a hobby i em va canviar la vida, solia procrastinar molt i Blender ho va canviar, m'encantava la forma en la qual és podia veure el progrés a través del temps, de vèrtex en vèrtex, va canviar la meva vida a millor. Actualment estic creant pàgines web per petites empreses de franc amb el fi d'ampliar el meu portafolis i aconseguir més coneixements.`,
+	'introduction-text7': 	`M'agrada pensar que vida és un tren sobre raïls, i que el nostre deure és assegurar-nos que aquest no caigui, mentre més disciplina, més brillarà el nostre tren.`,
+	'introduction-text8': 	`❝Jump back and get back a little more ❞`,
+	'introduction-text9':	`--The Qemists`,
+						
 	// Right
 
 	'introduction': 'Introducció',
